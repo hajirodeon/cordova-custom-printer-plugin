4bc1c2be4949415bc780bd2b4f0acbc22572b596
Carlos Adan Cortes De la Fuente <carlosadan@duplou.com>
2017-07-27 15:21:53 -0500

    * Cambio en el manejo de las funciones de Callback en el archivo de JS * Primer intento de implementación de impresión, es exitoso hasta el momento de revisar las impresoras detectadas

--- src/android/DuplouCustomPrinter.java
+++ src/android/DuplouCustomPrinter.java
@@ -1,12 +1,10 @@
 package com.duplou.cordova.plugin.customprinter;
 
 // CUSTOM Printer Imports
-/*
 import it.custom.printer.api.android.CustomPrinter;
 import it.custom.printer.api.android.CustomAndroidAPI;
 import it.custom.printer.api.android.PrinterFont;
 import it.custom.printer.api.android.CustomException;
-*/
 
 // Cordova Imports
 import org.apache.cordova.CordovaPlugin;
@@ -20,74 +18,33 @@ import org.json.JSONObject;
 // Android Imports
 import android.hardware.usb.UsbDevice;
 import android.content.Context;
-import android.util.Log;
 
 
 /**
-* This class echoes a string called from JavaScript.
+* This class prints tickets using CUSTOM Printer Java API
 */
 public class DuplouCustomPrinter extends CordovaPlugin {
-
-    @Override
-    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-        if (action.equals("echo")) {
-            String message = args.getString(0);
-            this.echo(message, callbackContext);
-            return true;
-        }
-        return false;
-    }
-
-    private void echo(String message, CallbackContext callbackContext) {
-        if (message != null && message.length() > 0) {
-            callbackContext.success(message);
-        } else {
-            callbackContext.error("Expected one non-empty string argument.");
-        }
-    }
-
-    /*
 
     // Initializes the avaliable usb printers connected to the device
     static UsbDevice[] usbDeviceList = null;
 
-    // Activity Context
-    private Context context = this.cordova.getActivity().getApplicationContext(); 
-
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-        
-        if (action.equals("echo")) {
-            String message = args.getString(0);
-            this.echo(message, callbackContext);
-            return true;
-        }
-
-        if (action.equals("print")){
+        if (action.equals("print")) {
             String text = args.getString(0);
             this.print(text, callbackContext);
             return true;
         }
-
         return false;
     }
 
-    // Test Function
-    private void echo(String message, CallbackContext callbackContext) {
-        Log.d("ECHO","Entro la funcion");
-        if (message != null && message.length() > 0) {
-            callbackContext.success(message);
-        } else {
-            callbackContext.error("Expected one non-empty string argument");
-        }
-    }
-
     // Print Function
-    private void print(String text, CallbackContext callbackContext){
-        Log.d("PRINT","Entro la funcion");
+    private void print(String text, CallbackContext callbackContext) {
+        // Checks if the string is empty
         if (text != null && text.length() > 0) {
-            // Checks the connected usb printers
             try {
+                // Activity Context
+                Context context = this.cordova.getActivity().getApplicationContext();
                 //Get the list of devices
                 usbDeviceList = CustomAndroidAPI.EnumUsbDevices(context);
 
@@ -95,7 +52,7 @@ public class DuplouCustomPrinter extends CordovaPlugin {
                     //Show Error
                     callbackContext.error("No connected printer found, please verify it is indeed connected");
                     return;
-                }else{
+                } else {
                     // Creates new printer object
                     CustomPrinter printer = new CustomAndroidAPI().getPrinterDriverUSB(usbDeviceList[0],context);
 
@@ -132,5 +89,4 @@ public class DuplouCustomPrinter extends CordovaPlugin {
             callbackContext.error("Expected one non-empty string argument.");
         }
     }
-    */
 }
--- www/duplou-custom-printer.js
+++ www/duplou-custom-printer.js
@@ -1,33 +1,17 @@
 var DuplouCustomPrinter = function(){};
 
-DuplouCustomPrinter.prototype.echo = function(str){
+// Java Class Name
+var PLUGIN_NAME = "DuplouCustomPrinter";
+
+DuplouCustomPrinter.prototype.print = function(text, successCallback, errorCallback){
     cordova.exec(
-        function(echoValue){alert(echoValue);},     // Success Callback
-        function(error){alert(error);},             // Error callback
-        "DuplouCustomPrinter",                      // Java Class
-        "echo",                                     // Java class method
-        [str]                                      // Arguments
+        successCallback,
+        errorCallback,
+        PLUGIN_NAME,
+        "print", // Native Method Name
+        [text] // Arguments
     );
 };
-
-/*
-DuplouCustomPrinter.prototype.echo =  function(message) {
-    cordova.exec(function(success){
-        alert(success);
-    }, function(error){
-        alert(error);
-    }, "DuplouCustomPrinter", "echo", [message]);
-};
-
-
-DuplouCustomPrinter.prototype.print =  function(text) {
-    cordova.exec(function(success){
-    	alert(success);
-    }, function(error){
-    	alert(error);
-    }, "DuplouCustomPrinter", "print", [text]);
-};
-*/
 
 var duplouCustomPrinter = new DuplouCustomPrinter();
 
